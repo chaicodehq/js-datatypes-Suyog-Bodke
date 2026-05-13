87d390110b9878a225166ad93708e4992eb50ab3
Suyog-Bodke <suyog1303@gmail.com>
2026-05-13 22:05:01 +0530

    1st assignment done

--- src/01-aadhaar-masker.js
+++ src/01-aadhaar-masker.js
@@ -29,4 +29,13 @@
  */
 export function maskAadhaar(aadhaarNumber) {
   // Your code here
+  if (typeof aadhaarNumber !== 'string' || aadhaarNumber.length !== 12 ) return "INVALID"
+
+  if (!/^\d+$/.test(aadhaarNumber)) {
+    return "INVALID";
+  }
+
+  const lastFourDigit = aadhaarNumber.slice(8);
+
+  return `XXXX-XXXX-${lastFourDigit}`
 }
